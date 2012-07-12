a6baa7c4e23670b2727e84b794671440f464c7a0
hitode909 <hitode909@gmail.com>
2012-07-12 14:00:16 +0900

    HSLランダムカラー

--- Bon3/ViewController.m
+++ Bon3/ViewController.m
@@ -72,7 +72,7 @@
     float green = arc4random() % 2;
     float blue = arc4random() % 2;
     red = red + green + blue >= 3 ? 0 : red;    
-    _groundView.backgroundColor = _ossanView.ossanColor = [UIColor colorWithRed:red green:green blue:blue alpha:1.0];
+    _groundView.backgroundColor = _ossanView.ossanColor = [UIColor colorWithHue: (float)(arc4random()%360) / 360.0 saturation:1.0 brightness:1.0 alpha:1.0];
     [self nextTrack];
     [self loadSamples];
 }
